446ba5f518946daf3e2937b67a1227168d231388
Danil Kuzebnyi <danilych77767@gmail.com>
2023-10-08 17:59:32 +0200

    create docker-compose.yml to run 2 containers

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM openjdk:11-jre-slim
-
-WORKDIR /app
-
-COPY build/libs/course_project-0.0.1-SNAPSHOT.jar /app/
-
-EXPOSE 8080
-
-CMD ["java", "-jar", "course_project-0.0.1-SNAPSHOT.jar"]
--- src/main/java/org/danylo/FitnessApplication.java
+++ src/main/java/org/danylo/FitnessApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class CourseProjectApplication {
+public class FitnessApplication {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(CourseProjectApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(FitnessApplication.class, args);
         FlywayConfiguration databaseMigration = applicationContext
                 .getBean("databaseMigration", FlywayConfiguration.class);
         databaseMigration.applyDatabaseMigrations();
